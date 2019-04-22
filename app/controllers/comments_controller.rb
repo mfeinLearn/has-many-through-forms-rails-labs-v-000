9eede51b4b6ba14589e6356f90070352d799a4ff
github username <github email address>
2019-04-22 16:33:16 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,6 @@
 class CommentsController < ApplicationController
 
-  def show
-    binding.pry
-  end
-  
+
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
